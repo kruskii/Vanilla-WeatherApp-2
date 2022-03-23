d5a295ddc6241ee8704023e9daab0c0ba36ce959
kruskii <97854170+kruskii@users.noreply.github.com>
2022-03-22 21:13:05 -0400

    added other weather html elements to JS API integration

--- src/app.js
+++ src/app.js
@@ -3,17 +3,29 @@
 // add axios to use ajax to html
 
 function showCity(response) {
-  console.log(response.data);
-  console.log(response.data.name);
-  console.log(response.data.main.temp);
+  // console.log(response.data);
+  // console.log(response.data.name);
+  // console.log(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let maxTempElement = document.querySelector("#max-temp");
+  let minTempElement = document.querySelector("#min-temp");
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  humidityElement.innerHTML = response.data.main.humidity;
+  maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
+  minTempElement.innerHTML = Math.round(response.data.main.temp_min);
 }
 
 let apiKey = `d956b0842cbabf1d3e0333b095cbca8d`;
-let city = `Paris`;
+let city = `Hamilton`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showCity);
