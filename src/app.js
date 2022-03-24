dbbc693ca0a04f297c0a18116024caf6b58bd61c
kruskii <97854170+kruskii@users.noreply.github.com>
2022-03-23 20:43:53 -0400

    added icon element query selector to JS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
 //add API integration, connect to OpenWeather App
 
-// add axios to use ajax to html
+// add axios to use ajax to connect to API
+
+//function that shows default city value
 
 function showCity(response) {
-  // console.log(response.data);
-  // console.log(response.data.name);
-  // console.log(response.data.main.temp);
   let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let windElement = document.querySelector("#wind");
@@ -14,6 +13,7 @@ function showCity(response) {
   let humidityElement = document.querySelector("#humidity");
   let maxTempElement = document.querySelector("#max-temp");
   let minTempElement = document.querySelector("#min-temp");
+  let iconElement = document.querySelector("#icon");
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -22,10 +22,14 @@ function showCity(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   maxTempElement.innerHTML = Math.round(response.data.main.temp_max);
   minTempElement.innerHTML = Math.round(response.data.main.temp_min);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 let apiKey = `d956b0842cbabf1d3e0333b095cbca8d`;
-let city = `Hamilton`;
+let city = `Houston`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showCity);
